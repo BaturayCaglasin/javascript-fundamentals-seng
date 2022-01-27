958bd96bb8cee572e1714d5c57b89d11b9cd133a
BaturayCaglasin <baturaycaglasin@gmail.com>
2022-01-27 23:32:12 +0300

    Commit-1

---
+++ starter/example.js
@@ -0,0 +1,41 @@
+/*
+let country = "Turkey";
+let continent = "Asia";
+let population = "1000000";
+let boolean = "true";
+let children; //undefined value
+
+console.log(country);
+console.log(continent);
+console.log(population);
+
+
+let javaScriptIsFun = true;
+javaScriptIsFun = "true";
+
+console.log(javaScriptIsFun);
+
+console.log(typeof javaScriptIsFun);
+console.log(typeof "Batu");
+console.log(typeof 27);
+
+let year;
+year = 1991;
+console.log(year);
+
+console.log(typeof null);
+
+*/
+
+
+let age = 30;
+age = 27;
+console.log(age);
+
+const birth = 1995; //const kullanıldığında artık value değiştirilemez.
+//birth = 1995;
+console.log(birth);
+
+var job = "programmer";
+job = "teacher";
+console.log(job);
---
+++ starter/script.js
@@ -0,0 +1,11 @@
+let js = 'amazing';
+if (js === 'amazing') alert('JavaScript is fun!');
+console.log(40 + 2 - 10);
+//alert("I am learning JavaScript and it is so fun!");
+console.log("Baturay");
+console.log(27);
+console.log(3 + 4);
+
+let firstName = "Batu";
+
+console.log(firstName);
